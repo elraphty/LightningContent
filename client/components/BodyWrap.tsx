399e965829ad66353e9d642046cab7cf23e4c7ff
elraphty <elraphtyofficial@gmail.com>
2022-06-27 22:51:29 +0100

    added dashboard wrapper

---
+++ client/components/BodyWrap.tsx
@@ -0,0 +1,36 @@
+import type { NextPage } from 'next';
+import { ReactNode, useEffect } from 'react';
+import { getFromStorage } from '../helpers/localstorage';
+import { useRouter } from 'next/router';
+import Sidebar from './Sidebar';
+import Topbar from './Topbar';
+import MobileNav from './MobileNav';
+
+type BodyProps = {
+   children: ReactNode
+}
+
+const BodyWrap: NextPage<BodyProps> = (props: BodyProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // const token = getFromStorage('token');
+    // if (!token) {
+    //   router.push('/login');
+    // }
+  }, [router])
+
+
+  return (
+    <>
+      <Sidebar />
+      <Topbar />
+      <MobileNav />
+      <div className='bodywrap'>
+        {props.children}
+      </div>
+    </>
+  )
+}
+
+export default BodyWrap;
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,19 +1,21 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import type {NextPage} from 'next'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import BodyWrap from '../components/BodyWrap';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <meta name="description" content="Lightning app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={''}>
+      <BodyWrap>
         <h1 className='text-red mt-5'>Hello Testing</h1>
-      </main>
+      </BodyWrap>
+
     </div>
   )
 }
